6164df9df2d3ed6f82ef4569f0c5ff02b0a4069e
Maria <maria150@mail.ru>
2021-04-11 01:16:31 +0300

    Tab navigation

--- App.js
+++ App.js
@@ -1,15 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
-export default function App() {
+const GREENCOLOR = '#009E4E';
+const GRAYCOLOR = '#929292';
+
+function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home!</Text>
     </View>
   );
 }
+
+function ScanScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Scan!</Text>
+    </View>
+  );
+}
+
+function ProfileScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Profile!</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color, size }) => {
+            let iconName;
+            if (route.name === 'Домой') iconName = 'ios-home';
+            else if (route.name === 'Сканировать') iconName = 'ios-scan-sharp';
+            else if (route.name === 'Профиль') iconName = 'ios-person';
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: GREENCOLOR,
+          inactiveTintColor: GRAYCOLOR,
+        }}
+      >
+        <Tab.Screen name='Домой' component={HomeScreen} />
+        <Tab.Screen name='Сканировать' component={ScanScreen} />
+        <Tab.Screen name='Профиль' component={ProfileScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
+
 
 const styles = StyleSheet.create({
   container: {
@@ -19,3 +69,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
